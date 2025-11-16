429258049bd50185035d5481962735c86f7bcba8
VaclavMoucha <vaclav8moucha@gmail.com>
2025-11-16 13:28:21 +0100

    first commit

---
+++ res/index.js
@@ -0,0 +1,37 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".SecondHeaderContainer");
+const button = document.getElementById("langButton");
+let lang = "cs";
+
+button.addEventListener("click", () => {
+  console.log("click");
+  if (lang === "cs") {
+    lang = "en";
+  } else {
+    lang = "cs";
+  }
+  document.querySelectorAll("[data-en]").forEach((el) => {
+    el.textContent = el.getAttribute(`data-${lang}`);
+  });
+   document.querySelectorAll("[data-en]").forEach((el) => {
+    if (el.tagName === "IMG") {
+      el.src = el.getAttribute(`data-${lang}`);
+    }
+  });
+});
+
+
+hamburger.addEventListener("click", () => {
+  console.log("click");
+  if (navMenu.style.display === "flex") {
+    navMenu.style.display = "none";
+  } else {
+    navMenu.style.display = "flex";
+  }
+});
+
+window.addEventListener("resize", () => {
+    if (window.innerWidth > 1520) {
+        navMenu.style.display = "none"; 
+    }
+});
