bc604c802e0ba6c216eeda2c9bf9057edcdea749
VaclavMoucha <vaclav8moucha@gmail.com>
2025-11-19 13:51:14 +0100

    added about department

--- res/index.js
+++ res/index.js
@@ -2,27 +2,37 @@ const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".SecondHeaderContainer");
 const button = document.getElementById("langButton");
 const button2 = document.getElementById("langButton2");
-const buttons = [button, button2];
-let lang = "cs";
 
-buttons.forEach(btn =>{btn.addEventListener("click", () => {
-  console.log("click");
-  if (lang === "cs") {
-    lang = "en";
-  } else {
-    lang = "cs";
-  }
+let lang = localStorage.getItem("lang") || "cs";
+
+const buttons = [button, button2];
+
+function updateLanguage() {
   document.querySelectorAll("[data-en]").forEach((el) => {
-    el.textContent = el.getAttribute(`data-${lang}`);
-  });
-   document.querySelectorAll("[data-en]").forEach((el) => {
     if (el.tagName === "IMG") {
       el.src = el.getAttribute(`data-${lang}`);
+      return;
     }
+
+    el.textContent = el.getAttribute(`data-${lang}`);
+  });
+}
+
+updateLanguage();
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    if (lang === "cs") {
+      lang = "en";
+    } else {
+      lang = "cs";
+    }
+
+    localStorage.setItem("lang", lang);
+
+    updateLanguage();
   });
 });
-});
-
 hamburger.addEventListener("click", () => {
   console.log("click");
   if (navMenu.style.display === "flex") {
@@ -33,7 +43,7 @@ hamburger.addEventListener("click", () => {
 });
 
 window.addEventListener("resize", () => {
-    if (window.innerWidth > 1520) {
-        navMenu.style.display = "none"; 
-    }
+  if (window.innerWidth > 1520) {
+    navMenu.style.display = "none";
+  }
 });
