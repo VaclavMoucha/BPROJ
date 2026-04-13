6a66cce59092a0ca96bf6be6943474f2c2f5d8cc
VaclavMoucha <vaclav8moucha@gmail.com>
2026-04-13 17:59:53 +0200

    fixed bug

--- res/js/header.js
+++ res/js/header.js
@@ -1,52 +1,47 @@
-function initHeader(){
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".dropdownMenu");
-const button = document.getElementById("langButton");
-const button2 = document.getElementById("langButton2");
+function initHeader() {
+  const hamburger = document.querySelector(".hamburger");
+  const navMenu = document.querySelector(".dropdownMenu");
+  const button = document.getElementById("langButton");
+  const button2 = document.getElementById("langButton2");
 
-let lang = localStorage.getItem("lang") || "cs";
+  let lang = localStorage.getItem("lang") || "cs";
 
-const buttons = [button, button2];
+  const buttons = [button, button2];
 
-function updateLanguage() {
-  document.querySelectorAll("[data-en]").forEach((el) => {
-    if (el.tagName === "IMG") {
-      el.src = el.getAttribute(`data-${lang}`);
-      return;
-    }
+  function updateLanguage() {
+    document.querySelectorAll("[data-en]").forEach((el) => {
+      el.textContent = el.getAttribute(`data-${lang}`);
+    });
+  }
 
-    el.textContent = el.getAttribute(`data-${lang}`);
+  updateLanguage();
+
+  buttons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      if (lang === "cs") {
+        lang = "en";
+      } else {
+        lang = "cs";
+      }
+
+      localStorage.setItem("lang", lang);
+      updateLanguage();
+      if (typeof renderList === "function") renderList();
+      if (typeof renderDetail === "function") renderDetail();
+    });
   });
-}
-
-updateLanguage();
-
-buttons.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    if (lang === "cs") {
-      lang = "en";
+  hamburger.addEventListener("click", () => {
+    
+    if (navMenu.style.display === "flex") {
+      navMenu.style.display = "none";
     } else {
-      lang = "cs";
+      navMenu.style.display = "flex";
     }
-
-    localStorage.setItem("lang", lang);
-    updateLanguage();
-    if (typeof renderList === 'function') renderList()
-    if (typeof renderDetail === 'function') renderDetail()
   });
-});
-hamburger.addEventListener("click", () => {
-  console.log("click");
-  if (navMenu.style.display === "flex") {
-    navMenu.style.display = "none";
-  } else {
-    navMenu.style.display = "flex";
-  }
-});
-window.addEventListener("resize", () => {
-  if (window.innerWidth > 900) {
-    navMenu.style.display = "none";
-  }
-});
+  window.addEventListener("resize", () => {
+    if (window.innerWidth > 900) {
+      navMenu.style.display = "none";
+    }
+  });
 }
-initHeader();
+
